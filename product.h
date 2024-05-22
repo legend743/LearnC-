f154bc10637dd1b5f85389d610757961a0f5eaa6
faisal <mohd.faisal@blubook.co.in>
2024-05-22 15:09:18 +0530

    user define header file

---
+++ product.h
@@ -0,0 +1,7 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+int product(int x,int y){
+    return x*y;
+}
+#endif 
---
+++ useofHeaderfile.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "product.h"
+using namespace std;
+main(){
+    cout<<product(10,20)<<":is your product";
+}
