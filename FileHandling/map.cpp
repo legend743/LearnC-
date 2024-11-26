78a3890d09190f25e7e78758489aa2c769cd23f6
Faisal <fmohd743@gmail.com>
2024-11-26 10:28:41 +0530

    map

---
+++ FileHandling/map.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+#include<iterator>
+#include<string>
+
+int main(){
+    std::map<int,std::string> contatcs_details;
+    auto it=contatcs_details.begin();
+    contatcs_details.insert(it,std::make_pair(1,"Faisal"));
+    contatcs_details.insert(it,{2,"Javed"});
+    contatcs_details.insert(it,{3,"Junaid"});
+
+    //Details of Map
+    for(const auto& [key,value]:contatcs_details){
+        std::cout<<key<<":"<<value<<std::endl;
+    }
+
+}
